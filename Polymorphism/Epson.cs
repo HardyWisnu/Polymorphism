b68cf78f3d49a32a8237f2b8df87527f19588c1a
HardyWisnu <hardywisnu@amikom.students.ac.id>
2022-06-12 13:05:14 +0700

    OOP Polymorphism

---
+++ Polymorphism/Epson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Epson : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Epson display dimension: 10*11");
+        }
+    }
+}
---
+++ Polymorphism/PrinterWindows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class PrinterWindows
+    {
+        public String printer { get; set; }
+        public virtual void Show()
+        {
+            Console.WriteLine("Print display dimension: ");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Print printer printing....");
+        }
+    }
+}
